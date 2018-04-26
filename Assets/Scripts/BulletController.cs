20e7cacf5d6785a63433d1dabb8074738ff22b38
dqntm123 <36214326+dqntm123@users.noreply.github.com>
2018-04-26 10:16:59 +0900

    Initial commit

---
+++ Assets/Scripts/BulletController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour {
+
+    public float speed = 10f;
+    public float fireRange = 300f;
+    public float damage = 10f;
+
+    private Transform tr;
+    private Vector3 spawnPoint;
+
+	void Start ()
+    {
+        tr = GetComponent<Transform>();//선언한 트랜스폼의 변수 tr에 이 스크립트가 달려있는 오브젝트의 트랜스폼을 집어넣는다.
+        spawnPoint = tr.position;//벡터3값으로 선언한 spawPoint에 따로 선언한 tr의 포지션값을 넣는다.
+	}
+	
+	
+	void Update ()
+    {
+        tr.Translate(Vector3.forward * Time.deltaTime * speed);//벡터3값의 forward(0,1,0)으로 변수로선언한 speed값만큼 계속 이동한다.
+        if((spawnPoint-tr.position).sqrMagnitude>fireRange)
+        {
+            Destroy(gameObject);
+        }
+	}
+}
---
+++ Assets/Scripts/photonIint.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class photonIint : MonoBehaviour {
+
+    public Text logText;
+
+	void Awake ()
+    {
+        //포톤네트웍에 버전별로 분리하여 접속
+        PhotonNetwork.ConnectUsingSettings("Photon 1.0");
+	}
+	
+	void OnJoinedLobby()
+    {
+        //로비에 입장하였을때 호출되는 콜백함수
+        Debug.Log("Joined Lobby!!!!!");
+        PhotonNetwork.JoinRandomRoom();
+    }
+    
+	void OnPhotonRandomJoinFailed()
+    {
+        //랜덤룸입장에 실패하엿을때 호출되는 콜백함수
+        Debug.Log("No Room");
+        PhotonNetwork.CreateRoom("MyRoon");
+    }
+    void OnCreatedRoom()
+    {
+        //룸을 생성완료 하였을때 호출되는 콜백함수
+        Debug.Log("Finish make a room");
+    }
+    
+    void OnJoinedRoom()
+    {
+        //룸에입장했을때 호출되는 콜백함수
+        Debug.Log("Joined Room");
+        StartCoroutine(CreatPlayer());
+    }
+
+    IEnumerator CreatPlayer()
+    {
+        PhotonNetwork.Instantiate("MyPlayer", new Vector3(0, 1, 0), transform.rotation, 0);
+        yield return null;
+    }
+    
+    void Update()
+    {
+        logText.text = PhotonNetwork.connectionStateDetailed.ToString();//포톤네트웍에 현재나의 커넥션 상황을 텏스트로 매프레임마다 받는다.
+    }
+}
